a35ecb37c1661304c4aa8709f3d7570b6486e9ab
JayaPrakashGande <48028679+JayaPrakashGande@users.noreply.github.com>
2020-02-17 16:41:33 +0100

    Main JS file

---
+++ src/demo.js
@@ -0,0 +1,37 @@
+(function () {
+  let template = document.createElement("template");
+  template.innerHTML = `<p id = "customWidgetText"></p>`;
+  
+  class Demo extends HTMLElement {
+    constructor () {
+      super();
+      let shadowHost = this.attachShadow({mode : "open"});
+      shadowHost.appendChild(template.content.cloneNode(true));
+      this.addEventListener("click", event => {
+        var event = new Event("onClick");
+        this.dispatchEvent(event);
+      });
+      var this._textElement = document.getElementByID("customWidgetText");
+      this._props = {};
+    }
+    
+    onCustomWidgetBeforeUpdate(changedProperties) {
+      this._props = {...this._props, ...changedProperties};
+    }
+    
+    onCustomWidgetAfterUpdate(changedProperties) {
+      if ("text" in changedProperties) {
+        this._textElement.innerHTML = changedProperties["text"];
+      }
+    }
+    
+    set text(newText) {
+      this._textElement.innerHTML = newText;
+    }
+    
+    get text() {
+      return this._textElement.innerHTML;
+    }
+  }
+  customElements.define(com.jp.demo, Demo);
+})();
